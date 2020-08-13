05346b1d6c83b10b1610df71c53e3355d3b97c5f
Aidan <aidankelly.cs@gmail.com>
2020-08-13 20:24:59 +0100

    created models

--- models.py
+++ models.py
@@ -1,1 +1,69 @@
+import os
+from sqlalchemy import Column, String, create_engine, Integer, Date
+from flask_sqlalchemy import SQLAlchemy
+import json
 
+database_path = os.environ['DATABASE_URL']
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+class Movie(db.Model):  
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    release_date = Column(Date)
+
+    def format(self):
+        return {
+        'id': self.id,
+        'title': self.name,
+        'release_date': self.catchphrase}
+    
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Actor(db.Model):  
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    gender = Column(String)
+
+    def format(self):
+        return {
+        'id': self.id,
+        'name': self.name,
+        'age': self.age,
+        'gender': self.gender}
+    
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
